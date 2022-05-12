bcd728d8a1231f056abf084b22ef5ef98500d297
David <101408842+lordasd@users.noreply.github.com>
2022-05-12 16:21:16 +0300

    Update ex3b.cc

--- Semester 2/ex3/ex3b.cc
+++ Semester 2/ex3/ex3b.cc
@@ -1,45 +1,14 @@
-/* Ex3b: Quarter sub_tree
+/* Ex3b: Quarter Sub_Tree
 *==================================
-* Written by David Zaydenberg
+* Written by David Zaydenberg, ID: 319323721, Login: davidzay
 *
-* Program takes input of Id, x, y and builds a node of of these parameters 
-* and create a binary search tree.
+* Program takes input of Id, x, y and builds a node of
+* of these parameters and create a binary search tree.
 * Program searches and checks which sub tree's point values
-* are in the same quadrent as both points of the left and right nodes
-* and saves the subtree that have the most of this condition.
-* ** RunTime - O(N) **
-* 
-* Main - Main calls functions.
-* Creates bst, Gets subtree, prints subtree and frees memory 
-*
-* Create_tree - Creates a binary tree. Input ID, x and y until eof(alt+z/d)
-* Node is created, data(x,y) in inserted in the node, and then
-* the node is inserted into the tree in insert_node().
-*
-* New_Node - Create a node. left+right = NULL
-*
-* New_Point - Creates a Point node.
-*
-* Insert_Node - Insert a node to a tree
-*
-* Get_Subtree - Prepares variables to use in recursion and
-* gets the subtree from "find_subtree" function
-*
-* Find_Subtree - Finds the leaves(first left, than right) and check each
-* node if the condition is met(same sign). 
-*
-* Check_Subtree -  checks if the conditions are met for a node.
-* If the _data values(x,y) are the same sign as the node below, count++.
-*
-* Check_xy_left - Check left side if the x,y values
-* are the same(negative/possitive)
-*
-* Check_xy_right - Check right side if the x,y values
-* are the same(negative/possitive)
-*
-* Print_Subtree - Prints the subtree intersection x,y parameters
-*
-* Free_Tree - Free all tree memory
+* are in the same quadrent as both points of the
+* left and right nodes and saves the subtree that has the
+* most nodes which comply with the condition.
+* ** RunTime - O(N) ** while searching in-order.
 *
 *==================================*/
 //-------Include_Section---------//
@@ -74,15 +43,23 @@ Node* get_subtree(Node *root);
 void find_subtree(Node *root, Node *&subtree, size_t& max_count,
                   size_t &lcount, size_t &rcount,
                   bool &left_fail, bool &right_fail);
-void check_subtree(Node* root, Node *&subtree, size_t &max_count,
-           size_t &lcount, size_t &rcount, bool &left_fail, bool &right_fail);
-bool check_xy_left(Node* root, bool &left_fail);
-bool check_xy_right(Node* root, bool &right_fail);
+int find_subtree(Node *root, Node *&subtree, int& max_count);
+int check_nodes(Node *root, Node* &subtree, int sub_left,
+                int sub_right, int &max_count);
+void replace_subtree(Node *root, Node* &subtree, int sub_left,
+                     int sub_right, int &max_count);
+int check_intersec(Node *root, Node* &subtree, int sub_left,
+                   int sub_right, int &max_count);
+int check_leaf(Node *root, Node* &subtree, int sub_left,
+               int sub_right, int &max_count);
+int check_edge(Node *root, int sub_left, int sub_right);
+bool check_xy_left(Node* root);
+bool check_xy_right(Node* root);
 void print_subtree(Node *subtree);
 void free_tree(Node *root);
 
 //Main calls functions.
-//Creates bst, Gets subtree, prints subtree and frees memory 
+//Creates bst, Gets subtree, prints subtree and frees memory
 //-------------Main-------------//
 int main()
 {
@@ -173,153 +150,174 @@ void insert_node(Node *&root, Node *node)
 //--------------Get_inter---------------//
 Node* get_subtree(Node *root)
 {
-    size_t max_count = 0, lcount = 0, rcount = 0;
-    bool left_fail = false, right_fail = false;
+    int max_count = 0, last_count;
     Node* subtree = nullptr;
-    find_subtree(root, subtree, max_count, lcount,
-               rcount, left_fail, right_fail);
-    return subtree;
+
+    last_count = find_subtree(root, subtree, max_count);
+    //checks if the return amount is the same as max_count,
+    //meaning subtree != root.
+    if(max_count == last_count)
+        return subtree;
+    else if(last_count > max_count && last_count >= 0)
+    //If last count retured bigger than max_count and subtree didn't fail
+        return root; //We are at root(meaning the whole tree is valid)
+    else
+        return subtree;
 }
 
 //Finds the leaves(first left, than right) and check each
-//node if the condition is met(same sign). 
+//node if the condition is met(same sign).
 //-----------------Find_Subtree---------------------//
-void find_subtree(Node *root, Node *&subtree, size_t& max_count,
-                  size_t &lcount, size_t &rcount,
-                  bool &left_fail, bool &right_fail)
-{  
-    if(root->_left != nullptr) //Go left until the leaf
-    {
-        find_subtree(root->_left, subtree, max_count, lcount,
-                   rcount, left_fail, right_fail);
-    }
-    if(root->_right != nullptr) //Go right until the leaf
-    {
-        //If after visiting left side we got right_fail,
-        //we change it because it will be considered as left side.
-        //(Happends after returning to an intersection)
-        if(!left_fail && right_fail)
-        {
-            left_fail = true;
-            right_fail = false;
-        }
-        find_subtree(root->_right, subtree, max_count, lcount,
-                   rcount, left_fail, right_fail);
-    }
-    //After finished visiting edges and we are at the intersection
-    //Checks if no edge failed saves the right count to the left and
-    //clears the right count so it will start a new count in the right subtree
-    if(root->_left != nullptr && root->_right != nullptr &&
-        rcount > lcount && max_count > 0 && !left_fail && !right_fail)
-    {
-        lcount = rcount;
-        rcount = 0;
-    }
-    //Goes into the node and checks if the conditions are met
-    check_subtree(root, subtree, max_count, lcount,
-                 rcount, left_fail, right_fail);
-    //If count is bigger than max_count save it and replace subtree.
-    if(lcount > max_count || rcount > max_count)
-    {
-        if(lcount > max_count){
-            subtree = root;
-            max_count = lcount;
-        }
-        else if(rcount > max_count){
-            subtree = root;
-            max_count = rcount;
-        }
-    }
-}
-
-//checks if the conditions are met for a node.
-//If the _data values(x,y) are the same sign as the node below, count++.
-//------------------Check_Subtree------------------//
-void check_subtree(Node* root, Node *&subtree, size_t &max_count,
-           size_t &lcount, size_t &rcount, bool &left_fail, bool &right_fail)
+int find_subtree(Node *root, Node *&subtree, int& max_count)
 {
-    //If we are at a leaf
-    if(root->_right == nullptr && root->_left == nullptr)
+    int sub_left = 0, sub_right = 0;
+
+    if(root == nullptr) //return 0 if at null
     {
-        if(rcount > 0){//left leaf visit
-            lcount++;
-        }
-        else if(lcount > 0){//right leaf visit
-            rcount++;
-        }
-        else{
-            lcount++;//First leaf visit
-        }
+        return 0;
     }
-    //If we are at a left subtree
-    else if(root->_right == nullptr && check_xy_left(root, left_fail))
+    //If we meet an intersection split sums to sub_left and right
+    if(root->_left != nullptr && root->_right != nullptr)
     {
-        lcount++;
+        sub_left += find_subtree(root->_left, subtree, max_count);
+        sub_right += find_subtree(root->_right, subtree, max_count);
     }
-    //If we are at a right subtree
-    else if(root->_left == nullptr && check_xy_right(root, right_fail))
+    //After reaching a leaf turn back and sum with sub_left or sub_right
+    if(root->_left != nullptr && root->_right == nullptr)
+        sub_left += find_subtree(root->_left, subtree, max_count);
+    else if(root->_left == nullptr && root->_right != nullptr)
+        sub_right += find_subtree(root->_right, subtree, max_count);
+    //after each node visit, check if subtree can be replaced
+    if(sub_left > max_count || sub_right > max_count)
+        replace_subtree(root, subtree, sub_left, sub_right, max_count);
+    //Check if a node meets the condition.
+    return check_nodes(root, subtree, sub_left, sub_right, max_count);
+}
+
+//Checks where is the node located and check the node itself for condition
+//-------------------Check_Nodes------------------------//
+int check_nodes(Node *root, Node* &subtree, int sub_left,
+                int sub_right, int &max_count)
+{
+    if(root->_left != nullptr && root->_right != nullptr)//intersection check
+        return check_intersec(root, subtree, sub_left, sub_right, max_count);
+    if(root->_left == nullptr && root->_right == nullptr)//Leaf check
+        return check_leaf(root, subtree, sub_left, sub_right, max_count);
+    if(root->_left != nullptr || root->_right != nullptr)//Nodes in edges
+        return check_edge(root, sub_left, sub_right);
+    return 0;
+}
+
+//Replace the subtree with the side that has more amount
+//--------------------Replace_Subtree----------------------//
+void replace_subtree(Node *root, Node* &subtree, int sub_left,
+                     int sub_right, int &max_count)
+{
+    if(sub_left > max_count)
     {
-        rcount++;
+        max_count = sub_left;
+        subtree = root->_left;
     }
-    //If we are at an intersection and both (left&right) subtrees
-    //are valid, sum them up and make the intersection as the subtree.
-    else if(root->_left != nullptr && root->_right != nullptr){
-        if(lcount > 0 && rcount > 0 && check_xy_left(root, left_fail) &&
-        check_xy_right(root, right_fail) && !left_fail && !right_fail){
-            max_count = lcount+rcount+1;
-            subtree = root;
-        }
-        return; //(go back(node before))
+    else
+    {
+        max_count = sub_right;
+        subtree = root->_right;
     }
 }
 
-//Check left side if the x,y values are the same(negative/possitive)
+//after visiting both sides we check the intersection and
+//---------------------Check_Intersec--------------------//
+int check_intersec(Node *root, Node* &subtree, int sub_left,
+                   int sub_right, int &max_count)
+{
+    if(sub_left >= 0 && sub_right >= 0)
+    {   //Both left and right nodes have the same sign values(x,y)
+        if(check_xy_left(root) && check_xy_right(root))
+        {
+            subtree = root;
+            max_count = sub_left + sub_right + 1;
+            return max_count;
+        }
+    }
+    return -100;//Else fail the subtree
+}
+
+//Check the leaf(if it's first time), else return +1.
+//------------------------Check_Leaf----------------//
+int check_leaf(Node *root, Node* &subtree, int sub_left,
+               int sub_right, int &max_count)
+{
+    if(sub_left == 0 && sub_right == 0 && max_count == 0) // first leaf
+    {   //First leaf, make it subtree.
+        subtree = root;
+        max_count = 1;
+        return 1;
+    }
+    else{
+        return 1; // return 1 anyways because it's a leaf
+    }
+}
+
+//Check if the values(x,y) of node in the edge are the same
+//sign as the node below.return +1 if yes, else fail the subtree 
+//--------------------Check_Edge--------------------//
+int check_edge(Node *root, int sub_left, int sub_right)
+{
+    if(sub_left >= 0 && sub_right >= 0)
+    {
+        if(root->_left != nullptr)
+        {
+            if(check_xy_left(root))
+                return 1 + sub_left;
+            else//fail the subtree
+                return -100 + sub_left;
+        }
+        else
+        {
+            if(check_xy_right(root))
+                return 1 + sub_right;
+            else//fail the subtree
+                return -100 + sub_left;
+        }
+    }
+    return -100;//Else fail the subtree
+}
+
+//Check left side if the x,y signs are the same(negative/possitive)
 //-----------------Check_xy_left------------------//
-bool check_xy_left(Node* root, bool &left_fail)
+bool check_xy_left(Node* root)
 {
     if(root->_data->_x < 0 && root->_left->_data->_x < 0)
     {
         if( (root->_data->_y < 0 && root->_left->_data->_y < 0) ||
-           (root->_data->_y > 0 && root->_left->_data->_y > 0) )
-        {
-            return true;
-        }
+            (root->_data->_y > 0 && root->_left->_data->_y > 0) )
+                return true;
     }
     else if(root->_data->_x > 0 && root->_left->_data->_x > 0)
     {
         if( (root->_data->_y < 0 && root->_left->_data->_y < 0) ||
-           (root->_data->_y > 0 && root->_left->_data->_y > 0) )
-        {
-            return true;
-        }
+            (root->_data->_y > 0 && root->_left->_data->_y > 0) )
+                return true;
     }
-    //Make left subtree failed, so there won't be intersection check.
-    left_fail = true; 
     return false;
 }
 
-//Check right side if the x,y values are the same(negative/possitive)
+//Check right side if the x,y signs are the same(negative/possitive)
 //-----------------Check_xy_Right-----------------//
-bool check_xy_right(Node* root, bool &right_fail)
+bool check_xy_right(Node* root)
 {
     if(root->_data->_x < 0 && root->_right->_data->_x < 0)
-    {   
+    {
         if( (root->_data->_y < 0 && root->_right->_data->_y < 0) ||
-           (root->_data->_y > 0 && root->_right->_data->_y > 0) )
-        {
-            return true;
-        }
-    }
+            (root->_data->_y > 0 && root->_right->_data->_y > 0) )
+                return true;
+    }            
     else if(root->_data->_x > 0 && root->_right->_data->_x > 0)
     {
         if( (root->_data->_y < 0 && root->_right->_data->_y < 0) ||
-           (root->_data->_y > 0 && root->_right->_data->_y > 0))
-        {
-            return true;
-        }
-    }
-    //Make left subtree failed, so there won't be intersection check.
-    right_fail = true;
+            (root->_data->_y > 0 && root->_right->_data->_y > 0) )
+                return true;
+    }           
     return false;
 }
 
@@ -332,7 +330,7 @@ void print_subtree(Node *subtree)
     }
     else{
         cout << subtree->_data->_x << " "
-        << subtree->_data->_y << endl;
+             << subtree->_data->_y << endl;
     }
 }
 
